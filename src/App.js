32e0283a96f10044dfbdc62bdf230f76aded0ccc
guneysrhat <guneysrhat@gmail.com>
2022-10-08 15:57:27 +0300

    added home navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./App.scss";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
